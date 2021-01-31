4729204d15130b33c4748b652a42da16c775108b
camu-pixel <66559584+camu-pixel@users.noreply.github.com>
2021-01-31 20:51:27 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,9 @@
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(0);
-}
-
-
-
 f=0;
 
 setup=_=>
 
 	{
-		createCanvas(1590,850)
+		createCanvas(1600,900)
 	};
 
 draw=_=>
@@ -24,17 +14,15 @@ draw=_=>
 
 		for(c=0;c<32;c++)
 		{
-			m=sin(((cos((f/150+((9-c)/18)+1)%1*PI*2)+1)/2)/1*(PI/2));
+			m=sin(((cos((f/150+((9-c)/200)+1)%1*PI*2)+1)/2)/1*(PI/2));
 
 			for(i=0;i<9;i++)
 	
 			{
 				stroke(i/9*300+80);
-				ellipse(c*50,((i/1.9)*m*200)+9,30);
+				ellipse(c*70,((i/1.9)*m*140)+40,60);
 			}
 		}
 	
 		f++
 	}
-
-
